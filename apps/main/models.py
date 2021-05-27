0e0b81c3a07bae7228974aa7c83ea71254c1b10e
Manav Bhatia <manavrbhatia@gmail.com>
2021-05-26 20:46:39 -0700

    remove old project

--- apps/main/models.py
+++ apps/main/models.py
@@ -14,10 +14,44 @@ def get_time():
     return datetime.datetime.utcnow()
 
 
-### Define your table below
-#
-# db.define_table('thing', Field('name'))
-#
-## always commit your models to avoid problems later
+db.define_table(
+    'user',
+    Field('user_email', default=get_user_email),
+    Field('first_name', requires=IS_NOT_EMPTY()),
+    Field('last_name', requires=IS_NOT_EMPTY()),
+    Field('age', 'integer'),
+    Field('location'),
+)
+
+db.define_table(
+    'propertyManager',
+    Field('name', requires=IS_NOT_EMPTY()),
+    Field('city', requires=IS_NOT_EMPTY()),
+    Field('state', requires=IS_NOT_EMPTY()),
+    Field('zip', 'integer'),
+    Field('bio'),
+)
+
+db.define_table(
+    'reviews',
+    Field('property_manager_id', 'reference propertyManager'),
+    Field('content', requires=IS_NOT_EMPTY()),
+    Field('upvotes', 'integer'),
+    Field('downvotes', 'integer'),
+    Field('user_id', 'reference user'),
+)
+
+db.define_table(
+    'savedReviews',
+    Field('user_id', 'reference user'),
+    Field('review_id', 'reference reviews'),
+)
+
+db.define_table(
+    'userRatingReview',
+    Field('user_id', 'reference user'),
+    Field('review_id', 'reference reviews'),
+    Field('upvoted', 'boolean'),
+)
 
 db.commit()
